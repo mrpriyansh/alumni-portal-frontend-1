f3557128bf998c42809273ba05fd4daa435455df
Gaharana <mr.priyanshgaharana@gmail.com>
2020-05-16 18:49:02 +0530

    change apiUrl

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const apiUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://alumni-backup-dot-tutorial-262713.el.r.appspot.com'
+    ? 'https://alumni-portal-backend-dot-tutorial-262713.el.r.appspot.com'
     : 'http://localhost:4000';
 console.log(apiUrl, process.env);
 const config = {
